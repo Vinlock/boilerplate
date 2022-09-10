d0c9435767a8320d87b957020dffca9e3ca03054
Dak Washbrook <Vinlock@users.noreply.github.com>
2022-09-10 10:50:36 -0700

    Enable superjson

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const { withSuperjson } = require('next-superjson')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withSuperjson()(nextConfig)
