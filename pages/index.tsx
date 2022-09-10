f0a38b9088d793272662f08a8b366e7bca44a1cc
Dak Washbrook <Vinlock@users.noreply.github.com>
2022-09-09 23:20:53 -0700

    Login works

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,22 @@
-import type { GetServerSideProps, NextPage } from 'next'
-import { useSession, getProviders, signIn } from 'next-auth/react'
+import type { NextPage } from 'next'
+import { useSession, signIn } from 'next-auth/react'
 
-const Home: NextPage<Props> = (props) => {
+const Home: NextPage = () => {
 	const session = useSession()
 
 	return (
 		<div className="p-10">
-			{Object.values(props.providers || {}).map((provider) => {
-				return (
-					<button
-						onClick={async () => {return signIn(provider.id)}}
-						className="rounded-md bg-slate-500 text-white py-2 px-5"
-					>
-						Login with {provider.name}
-					</button>
-				)
-			})}
+			<button
+				onClick={async () => {
+					return signIn('auth0')
+				}}
+				className="rounded-md bg-slate-500 text-white py-2 px-5"
+			>
+				Login
+			</button>
 			{session.data?.user?.name}
 		</div>
 	)
-}
-
-type Props = {
-	providers: Awaited<ReturnType<typeof getProviders>>
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-	const providers = await getProviders()
-	return {
-		props: { providers },
-	}
 }
 
 export default Home
