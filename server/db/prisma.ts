a8cd5aa2f4f917f320999ec2852080c9e7dc07d0
Dak Washbrook <Vinlock@users.noreply.github.com>
2022-09-09 22:28:31 -0700

    Import models

--- server/db/prisma.ts
+++ server/db/prisma.ts
@@ -1,6 +1,10 @@
 export type {
-	// Import Models here so they are exported from the same place
+	// Import Models here, so they are exported from the same place
 	// and this file can naturally run as needed.
+	Account,
+	Session,
+	User,
+	VerificationToken,
 } from '@prisma/client'
 import { PrismaClient } from '@prisma/client'
 
