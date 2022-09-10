1888b0a33b37c13e100a32599319c098187cda71
Dak Washbrook <Vinlock@users.noreply.github.com>
2022-09-10 11:01:31 -0700

    Main Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,35 @@
 import type { NextPage } from 'next'
 import { useSession, signIn } from 'next-auth/react'
 import { trpc } from '~hooks/trpc'
+import MainLayout from '~layouts/MainLayout'
 
 const Home: NextPage = () => {
 	const session = useSession()
 	const { data, isLoading } = trpc.useQuery(['ping.pong'])
 
 	return (
-		<div className="p-10">
-			<div>
-				<button
-					onClick={async () => {
-						return signIn('auth0')
-					}}
-					className="rounded-md bg-slate-500 text-white py-2 px-5"
-				>
-					Login
-				</button>
-				<p>{session.data?.user?.name}</p>
+		<MainLayout>
+			<div className="p-10">
+				<div>
+					<button
+						onClick={async () => {
+							return signIn('auth0')
+						}}
+						className="rounded-md bg-slate-500 text-white py-2 px-5"
+					>
+						Login
+					</button>
+					<p>{session.data?.user?.name}</p>
+				</div>
+				<div>
+					{isLoading ? (
+						<p>Loading...</p>
+					) : (
+						<p>Ping is {data}</p>
+					)}
+				</div>
 			</div>
-			<div>
-				{isLoading ? (
-					<p>Loading...</p>
-				) : (
-					<p>Ping is {data}</p>
-				)}
-			</div>
-		</div>
+		</MainLayout>
 	)
 }
 
